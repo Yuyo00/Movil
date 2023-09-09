f639dd2c6f1867f5b4cac4f088ea7ab5b3336699
Antonia Mardones <ant.mardones@duocuc.cl>
2023-09-09 17:01:29 -0300

    Final :D

--- src/app/pages/correcto/correcto.page.ts
+++ src/app/pages/correcto/correcto.page.ts
@@ -13,31 +13,25 @@ export class CorrectoPage implements OnInit {
 
 
   constructor(
-        private activeroute: ActivatedRoute // Permite obtener los parámetros de la página login
-      , private router: Router) { // Permite crear animaciones con  
+        private activeroute: ActivatedRoute, private router: Router) {
 
     this.usuario = new Usuario('', '', '', '', '', '')
 
-    // Se llama a la ruta activa y se obtienen sus parámetros mediante una subscripcion
     this.activeroute.queryParams.subscribe(params => { 
 
       const nav = this.router.getCurrentNavigation();
       if (nav) {
-        // Si tiene datos extra, se rescatan y se asignan a una propiedad
         if (nav.extras.state) {
           this.usuario = nav.extras.state['usuario'];
           return;
         }
       }
-      // Si no vienen datos extra desde la página anterior, quiere decir que el usuario
-      // intentó entrar directamente a la página home sin pasar por el login,
-      // de modo que el sistema debe enviarlo al login para que inicie sesión.
-      
-
     });
   }
   public ngOnInit(): void {
-
   }
 
+  public iniciarSesion(): void{
+    this.router.navigate(['/login'])
+  }
 }
